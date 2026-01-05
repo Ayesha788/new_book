93398ad8e3002ef18e09d264a485f840215aed1f
Ayesha788 <164652720+Ayesha788@users.noreply.github.com>
2026-01-05 14:18:26 +0500

    Change base URL in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   url: 'https://ayesha788.github.io',
-  baseUrl: '/new_book/',
+  baseUrl: '/',
   trailingSlash: false,
 
   organizationName: 'Ayesha788',
